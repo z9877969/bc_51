12475811c208bf915088cb221bac9ffce23eb86d
z9877969 <z9877969@ukr.net>
2023-06-21 10:59:49 +0300

    adds categoriesMap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./index.scss";
 
+import * as icons from "./assets/img"; // {products, auto, healht}
+
 import { persistor, store } from "./redux/store";
 
 import App from "./components/App/App";
@@ -25,3 +27,40 @@ root.render(
   </Provider>
   // </React.StrictMode>
 );
+
+const data = {
+  продукти: {
+    total: 152,
+    p1: 12,
+    p2: 32,
+  },
+  авто: {
+    total: 152,
+    p1: 12,
+    p2: 32,
+  },
+  здоровье: {
+    total: 152,
+    p1: 12,
+    p2: 32,
+  },
+};
+
+const categoriesMap = {
+  продукти: {
+    label: "Products",
+    icon: "products",
+  },
+  авто: {
+    label: "Auto",
+    icon: "auto",
+  },
+  здоровье: "Health",
+};
+
+const categoriesList = Object.entries(data).map(([key, value]) => ({
+  categoryName: categoriesMap[key].label,
+  categoryIcon: icons[categoriesMap[key].icon],
+}));
+
+console.log("categoriesList :>> ", categoriesList);
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,2 +1,26 @@
 export { default as Header } from "./header/Header";
 export { AuthForm } from "./authPage/components/AuthForm/AuthForm";
+
+// const state = {
+//   auth: {
+//     user: {
+//       email: null,
+//       balance: 0,
+//     },
+//     sid: null,
+//     accessToken: null,
+//     refreshToken: null,
+//     error: null,
+//     isLoading: null,
+//   },
+//   transactions: {
+//     income: {
+//       transactions: [],
+//       stats: [],
+//     },
+//     expences: {
+//       transactions: [],
+//       stats: [],
+//     },
+//   },
+// };
